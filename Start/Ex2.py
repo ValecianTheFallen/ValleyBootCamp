c70ef8114162cc1e054bcdd751a04a8da298054c
Varun.G <var2797@gmail.com>
2018-07-09 13:22:02 -0700

    Example 2 done

---
+++ Start/Ex2.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def WWE():
+	return 'Welcome WWE Universe'
+
+@app.route('/TLC')
+def TLC():
+   	return 'Welcome To TLC 2018\nDo Not Try This At Home' % name
+
+@app.route('/TLC/<match>')
+def hello_name(match):
+	return 'Its %s!' % match
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=2002 , debug = True)
+
