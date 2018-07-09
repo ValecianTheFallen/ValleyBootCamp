f7028af75ab6ea0ccae3a3ac67ad83b2980e6c4e
Varun.G <var2797@gmail.com>
2018-07-09 13:37:08 -0700

    Changes To Ex1

--- Start/Ex1.py
+++ Start/Ex1.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/hello/')
+def hello():
+   return 'Hello!' 
+
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
